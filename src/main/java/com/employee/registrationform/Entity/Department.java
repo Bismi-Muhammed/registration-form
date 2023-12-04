d7a0f1e685b97169660f0c803fbeef6398d4e24f
Bismi-Muhammed <152490883+Bismi-Muhammed@users.noreply.github.com>
2023-12-04 12:34:15 +0400

    Update Department.java

--- src/main/java/com/employee/registrationform/Entity/Department.java
+++ src/main/java/com/employee/registrationform/Entity/Department.java
@@ -1,36 +1,58 @@
 package com.employee.registrationform.Entity;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
 
 @Entity
-@Table(name = "department")
-public class Department {
+public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private String code;
-	private String departmentName;
-	@OneToMany(mappedBy = "department")
-    private List<Employee> employees;
 
-	public Department() {
+	@Size(max = 10, message = "Employee Number must be at most 10 characters")
+	@Pattern(regexp = "\\d+", message = "Value should contain only numeric characters")
+	@Column(name = "employee_no")
+	private String empNo;
+
+	@Size(max = 100, message = "Employee Nmae must be at most 100 characters")
+	@Column(name = "employee_name")
+	private String empName;
+
+	@Column(name = "date_of_joining")
+	private LocalDate joiningDate;
+
+	@Column(name = "department")
+	private int department;
+
+	@DecimalMin(value = "0", inclusive = true, message = "Value must be greater than or equal to 0")
+	@DecimalMax(value = "9999999999", inclusive = true, message = "Value must have a maximum of 10 digits")
+	private BigDecimal salary;
+
+	public Employee() {
 
 	}
 
-	public Department(Long id, String code, String departmentName, List<Employee> employees) {
+	public Employee(Long id, String empNo, String empName, LocalDate joiningDate, int department, BigDecimal salary) {
 		super();
 		this.id = id;
-		this.code = code;
-		this.departmentName = departmentName;
-		this.employees = employees;
+		this.empNo = empNo;
+		this.empName = empName;
+		this.joiningDate = joiningDate;
+		this.department = department;
+		this.salary = salary;
 	}
 
 	public Long getId() {
@@ -41,36 +63,51 @@ public class Department {
 		this.id = id;
 	}
 
-	public String getCode() {
-		return code;
+	public String getEmpNo() {
+		return empNo;
 	}
 
-	public void setCode(String code) {
-		this.code = code;
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
 	}
 
-	public String getDepartmentName() {
-		return departmentName;
+	public String getEmpName() {
+		return empName;
 	}
 
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
+	public void setEmpName(String empName) {
+		this.empName = empName;
 	}
 
-	public List<Employee> getEmployees() {
-		return employees;
+	public LocalDate getJoiningDate() {
+		return joiningDate;
 	}
 
-	public void setEmployees(List<Employee> employees) {
-		this.employees = employees;
+	public void setJoiningDate(LocalDate joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+
+	public int getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(int department) {
+		this.department = department;
+	}
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", code=" + code + ", departmentName=" + departmentName + ", employees="
-				+ employees + "]";
+		return "Employee [id=" + id + ", empNo=" + empNo + ", empName=" + empName + ", joiningDate=" + joiningDate
+				+ ", department=" + department + ", salary=" + salary + "]";
 	}
-
-	
 
 }
