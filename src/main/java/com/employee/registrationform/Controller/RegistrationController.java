3b1d533a1ae4a03621c8beb5876115c8e72d24e0
Bismi-Muhammed <152490883+Bismi-Muhammed@users.noreply.github.com>
2023-12-04 12:32:25 +0400

    Update RegistrationController.java

--- src/main/java/com/employee/registrationform/Controller/RegistrationController.java
+++ src/main/java/com/employee/registrationform/Controller/RegistrationController.java
@@ -1,22 +1,16 @@
 package com.employee.registrationform.Controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.employee.registrationform.Entity.Department;
-import com.employee.registrationform.Entity.Employee;
 import com.employee.registrationform.Service.EmployeeRegService;
 
 @Controller
@@ -36,25 +30,12 @@ public class RegistrationController {
 		model.addAttribute("departments", departments);
 		return "registration";
 	}
-
-
-//	  Post mapping to save the employee registration details into table Using
-//	  request parameters and after saving details return a registration successful page	 
-
-	@PostMapping("/saveEmployee")
-	public String registerEmployee(@Valid @RequestParam String empName, @RequestParam String employeeNo,
-			@RequestParam String joiningDate, @RequestParam Integer department, @RequestParam BigDecimal salary) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate localDate = LocalDate.parse(joiningDate, formatter);
-
-		Employee employee = new Employee();
-		employee.setEmpNo(employeeNo);
-		employee.setEmpName(empName);
-		employee.setJoiningDate(localDate);
-		employee.setDepartment(department);
-		employee.setSalary(salary);
-		empService.addRegistrationDetails(employee);
+// method to call success page upon registration completion
+	@RequestMapping(value="/success",method = RequestMethod.GET)
+	public String successPage() {
 		return "success";
 	}
+
+
 
 }
